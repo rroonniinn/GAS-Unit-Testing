3d5ce8bef03e88e15b46f031708dc306811ee2be
Ron (rroonniinn) <g.mogilewski@gmail.com>
2020-03-05 18:05:43 +0100

    Nowe testy

--- __src_Gas/index.js
+++ __src_Gas/index.js
@@ -1,12 +1,17 @@
-import {} from './env';
 import { getRangeType } from '../../GAS | Library/v02/gas/qunit/getRangeType';
 import { getLastNotEmptyRowInCol } from '../../GAS | Library/v02/gas/qunit/getLastNotEmptyRowInCol';
 import { getLastNotEmptyColInRow } from '../../GAS | Library/v02/gas/qunit/getLastNotEmptyColInRow';
 import { getRangeRelative } from '../../GAS | Library/v02/gas/qunit/getRangeRelative';
+import { isColumn } from '../../GAS | Library/v02/gas/qunit/isColumn';
+import { getRangeRestricted } from '../../GAS | Library/v02/gas/qunit/getRangeRestricted';
+
+import {} from './env';
 
 global.tests = () => {
-	// getRangeType();
+	// isColumn();
 	// getLastNotEmptyRowInCol();
 	// getLastNotEmptyColInRow();
+	// getRangeType();
 	getRangeRelative();
+	// getRangeRestricted();
 };
